5b5b62c17c8be25d8c0c8c57bd4a1f9ae18432b5
Vladislav <vladsermihailenko@gmail.com>
2022-05-17 16:55:30 +0200

    added conftest

--- 3_6_9/conftest.py
+++ 3_6_9/conftest.py
@@ -1,17 +1,18 @@
 import pytest
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="ru",
+    parser.addoption('--language', action='store', default="en",
                      help="Choose language")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
-    link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
-    browser = webdriver.Chrome()
-    browser.get(link)
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(10)
     yield browser
     browser.quit()
--- 3_6_9/test_items.py
+++ 3_6_9/test_items.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.common.by import By
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_add_to_cart(browser):
+    browser.get(link)
     button = browser.find_elements(by=By.CSS_SELECTOR, value="button.btn-add-to-basket")
     assert button, "Кнопки нет!"
